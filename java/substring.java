573f2a85ef294db94ed65ad0d483bf1f36f5577a
Choi TaeSan <xotks7524@gmail.com>
2024-07-07 21:41:00 +0900

    부분 문자열 문제

---
+++ java/substring.java
@@ -0,0 +1,14 @@
+public class substring {
+    public static int solution(String str1, String str2) {
+        if (str2.contains(str1)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("abc", "aabcc"));
+        System.out.println(solution("tbt", "tbbttb"));
+    }
+}
