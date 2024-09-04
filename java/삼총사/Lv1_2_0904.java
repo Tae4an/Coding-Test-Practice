0752adf607ce818b54736e0bfd3e1e59d60a955d
TaeSan Choi / 최태산 <xotks7524@gmail.com>
2024-09-04 19:37:00 +0900

    삼총사 문제

---
+++ java/삼총사/Lv1_2_0904.java
@@ -0,0 +1,25 @@
+package 삼총사;
+
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Arrays;
+
+public class Lv1_2_0904 {
+    public static void main(String[] args) {
+        Lv1_2_0904 sol = new Lv1_2_0904();
+        int[] arr = {-3, -2, -1, 0, 1, 2, 3};
+        int result = sol.solution(arr);
+        System.out.println(result);
+    }
+    public int solution(int[] number) {
+        int cnt = 0, result = 0;
+        for (int i = 2; i < number.length; i++) {
+            for (int j = i; j < number.length; j++) {
+                for (int k = j; k < number.length; k++) {
+                    result = number[i-2] + number[j-1] + number[k];
+                    if (result == 0) cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
