8dea8704b8097bb9f2243e4999c114fcef228d09
TaeSan Choi / 최태산 <xotks7524@gmail.com>
2024-10-12 19:00:31 +0900

    원소들의 곱과 합 문제

---
+++ java/원소들의곱과합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int product = 1;
+        
+        for (int num : num_list) {
+            sum += num;
+            product *= num;
+        }
+        
+        int squareOfSum = sum * sum;
+        
+        return (product < squareOfSum) ? 1 : 0;
+    }
+}
